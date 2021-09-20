ffc1b4268c2b821cde51c8278a22488aa0819a33
Olivier Henry <olivierpascalhenry>
2021-09-20 15:22:31 +0200

    update 0.6.0 - add missing files

---
+++ ui/Ui_downloadwindow.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'downloadwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_downloadWindow(object):
+    def setupUi(self, downloadWindow):
+        downloadWindow.setObjectName("downloadWindow")
+        downloadWindow.resize(550, 166)
+        downloadWindow.setMinimumSize(QtCore.QSize(0, 0))
+        downloadWindow.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        downloadWindow.setStyleSheet("QWidget#downloadWindow {\n"
+"    background-color: rgb(230,230,230);\n"
+"}")
+        self.gridLayout = QtWidgets.QGridLayout(downloadWindow)
+        self.gridLayout.setObjectName("gridLayout")
+        self.dw_progress_bar = QtWidgets.QProgressBar(downloadWindow)
+        self.dw_progress_bar.setMinimumSize(QtCore.QSize(0, 30))
+        self.dw_progress_bar.setMaximumSize(QtCore.QSize(16777215, 30))
+        font = QtGui.QFont()
+        font.setFamily("Source Sans Pro")
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setWeight(50)
+        font.setKerning(True)
+        font.setStyleStrategy(QtGui.QFont.PreferAntialias)
+        self.dw_progress_bar.setFont(font)
+        self.dw_progress_bar.setStyleSheet("QProgressBar {\n"
+"   border: 0px solid black;\n"
+"   border-radius: 3px;\n"
+"   background: rgb(220,220,220);\n"
+"   color: rgb(45,45,45);\n"
+"   text-align: center;\n"
+"}\n"
+"\n"
+"QProgressBar::chunk {\n"
+"   border: 0px solid black;\n"
+"   border-radius: 3px;\n"
+"   background: rgb(0,200,0);\n"
+" }")
+        self.dw_progress_bar.setProperty("value", 0)
+        self.dw_progress_bar.setTextVisible(True)
+        self.dw_progress_bar.setObjectName("dw_progress_bar")
+        self.gridLayout.addWidget(self.dw_progress_bar, 0, 0, 1, 1)
+        self.dw_label = QtWidgets.QLabel(downloadWindow)
+        self.dw_label.setMinimumSize(QtCore.QSize(0, 50))
+        self.dw_label.setMaximumSize(QtCore.QSize(16777215, 50))
+        font = QtGui.QFont()
+        font.setFamily("Source Sans Pro")
+        font.setPointSize(12)
+        font.setItalic(True)
+        font.setKerning(True)
+        font.setStyleStrategy(QtGui.QFont.PreferAntialias)
+        self.dw_label.setFont(font)
+        self.dw_label.setStyleSheet("QLabel {\n"
+"    margin-top: 5px;\n"
+"    color: rgb(45,45,45);\n"
+"}")
+        self.dw_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.dw_label.setWordWrap(True)
+        self.dw_label.setObjectName("dw_label")
+        self.gridLayout.addWidget(self.dw_label, 1, 0, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem, 2, 0, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.dw_button = QtWidgets.QToolButton(downloadWindow)
+        self.dw_button.setMinimumSize(QtCore.QSize(100, 40))
+        self.dw_button.setMaximumSize(QtCore.QSize(100, 40))
+        font = QtGui.QFont()
+        font.setFamily("Source Sans Pro")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setWeight(50)
+        font.setKerning(True)
+        font.setStyleStrategy(QtGui.QFont.PreferDefault)
+        self.dw_button.setFont(font)
+        self.dw_button.setStyleSheet("QToolButton {\n"
+"    border: 1px solid #acacac;\n"
+"    border-radius: 1px;\n"
+"    background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1,\n"
+"                                    stop:0 #f0f0f0, stop:1 #e5e5e5);\n"
+"    color: rgb(45,45,45);\n"
+"}\n"
+"\n"
+"QToolButton:hover {\n"
+"    border: 1px solid #7eb4ea;\n"
+"    border-radius: 1px;\n"
+"    background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1,\n"
+"                                    stop:0 #ecf4fc, stop:1 #dcecfc);\n"
+"}\n"
+"\n"
+"QToolButton:pressed {\n"
+"    border: 1px solid #579de5;\n"
+"    border-radius: 1px;\n"
+"    background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1,\n"
+"                                    stop:0 #daecfc, stop:1 #c4e0fc);\n"
+"}")
+        self.dw_button.setObjectName("dw_button")
+        self.horizontalLayout.addWidget(self.dw_button)
+        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem2)
+        self.gridLayout.addLayout(self.horizontalLayout, 3, 0, 1, 1)
+
+        self.retranslateUi(downloadWindow)
+        QtCore.QMetaObject.connectSlotsByName(downloadWindow)
+
+    def retranslateUi(self, downloadWindow):
+        _translate = QtCore.QCoreApplication.translate
+        self.dw_progress_bar.setFormat(_translate("downloadWindow", "%p %"))
+        self.dw_label.setText(_translate("downloadWindow", "Downloading..."))
+        self.dw_button.setText(_translate("downloadWindow", "Cancel"))
---
+++ ui/Ui_updatewindow.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'updatewindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_updateWindow(object):
+    def setupUi(self, updateWindow):
+        updateWindow.setObjectName("updateWindow")
+        updateWindow.resize(660, 240)
+        updateWindow.setMinimumSize(QtCore.QSize(0, 0))
+        updateWindow.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("FreeSans")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        font.setKerning(True)
+        font.setStyleStrategy(QtGui.QFont.PreferAntialias)
+        updateWindow.setFont(font)
+        updateWindow.setStyleSheet("QWidget#updateWindow {\n"
+"    background-color: rgb(230,230,230);\n"
+"}")
+        self.gridLayout = QtWidgets.QGridLayout(updateWindow)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label = QtWidgets.QLabel(updateWindow)
+        self.label.setMinimumSize(QtCore.QSize(0, 0))
+        self.label.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("Source Sans Pro")
+        font.setPointSize(16)
+        font.setKerning(True)
+        font.setStyleStrategy(QtGui.QFont.PreferAntialias)
+        self.label.setFont(font)
+        self.label.setStyleSheet("QLabel {\n"
+"    color: rgb(45,45,45);\n"
+"}")
+        self.label.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.label.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.label.setLineWidth(0)
+        self.label.setMidLineWidth(0)
+        self.label.setTextFormat(QtCore.Qt.AutoText)
+        self.label.setAlignment(QtCore.Qt.AlignJustify|QtCore.Qt.AlignTop)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(20, 1, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem, 1, 0, 1, 1)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.ok_button = QtWidgets.QToolButton(updateWindow)
+        self.ok_button.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.ok_button.sizePolicy().hasHeightForWidth())
+        self.ok_button.setSizePolicy(sizePolicy)
+        self.ok_button.setMinimumSize(QtCore.QSize(50, 50))
+        self.ok_button.setMaximumSize(QtCore.QSize(50, 50))
+        font = QtGui.QFont()
+        font.setFamily("Source Sans Pro SemiBold")
+        font.setPointSize(22)
+        font.setKerning(True)
+        font.setStyleStrategy(QtGui.QFont.PreferAntialias)
+        self.ok_button.setFont(font)
+        self.ok_button.setStyleSheet("QToolButton {\n"
+"    border: 0px solid rgb(75,75,75);\n"
+"    border-radius: 3px;\n"
+"    background-color: transparent;\n"
+"    width: 27px;\n"
+"    height: 27px;\n"
+"    color: rgb(45,45,45);\n"
+"}\n"
+"\n"
+"QToolButton:flat {\n"
+"    border: none;\n"
+"}")
+        self.ok_button.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("icons/validate_icon.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.ok_button.setIcon(icon)
+        self.ok_button.setIconSize(QtCore.QSize(45, 45))
+        self.ok_button.setObjectName("ok_button")
+        self.horizontalLayout_3.addWidget(self.ok_button)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem1)
+        self.cancel_button = QtWidgets.QToolButton(updateWindow)
+        self.cancel_button.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.cancel_button.sizePolicy().hasHeightForWidth())
+        self.cancel_button.setSizePolicy(sizePolicy)
+        self.cancel_button.setMinimumSize(QtCore.QSize(50, 50))
+        self.cancel_button.setMaximumSize(QtCore.QSize(50, 50))
+        font = QtGui.QFont()
+        font.setFamily("Source Sans Pro SemiBold")
+        font.setPointSize(22)
+        font.setKerning(True)
+        font.setStyleStrategy(QtGui.QFont.PreferAntialias)
+        self.cancel_button.setFont(font)
+        self.cancel_button.setStyleSheet("QToolButton {\n"
+"    border: 0px solid rgb(75,75,75);\n"
+"    border-radius: 3px;\n"
+"    background-color: transparent;\n"
+"    width: 27px;\n"
+"    height: 27px;\n"
+"    color: rgb(45,45,45);\n"
+"}\n"
+"\n"
+"QToolButton:flat {\n"
+"    border: none;\n"
+"}")
+        self.cancel_button.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("icons/del_icon.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.cancel_button.setIcon(icon1)
+        self.cancel_button.setIconSize(QtCore.QSize(45, 45))
+        self.cancel_button.setObjectName("cancel_button")
+        self.horizontalLayout_3.addWidget(self.cancel_button)
+        spacerItem2 = QtWidgets.QSpacerItem(278, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem2)
+        self.gridLayout.addLayout(self.horizontalLayout_3, 2, 0, 1, 1)
+
+        self.retranslateUi(updateWindow)
+        QtCore.QMetaObject.connectSlotsByName(updateWindow)
+
+    def retranslateUi(self, updateWindow):
+        _translate = QtCore.QCoreApplication.translate
+        self.label.setText(_translate("updateWindow", "<html><head/><body><p align=\"justify\">Voulez-vous mettre à jour la station météo ?</p><p align=\"justify\">Une fois le processus lancé, il est fortement conseillé de ne pas interrompre le processus de quelque manière que ce soit.</p></body></html>"))
