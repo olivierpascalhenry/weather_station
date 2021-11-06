1fbd94b6088fd86173fa557bb1f03218ba09fe2a
Olivier Henry <olivierpascalhenry>
2021-11-06 09:26:03 +0100

    a new icon for fog has been added

--- functions/utils.py
+++ functions/utils.py
@@ -126,7 +126,7 @@ def weather_to_pictogrammes(weather, gui_path=''):
                          'Risque de grèle': 'risque_grele.svg', 'Averses orageuses': 'averses_orageuses.svg',
                          'Brume': 'brume.svg', 'Averses': 'averses.svg', 'Pluie': 'pluie.svg',
                          'Pluies orageuses': 'pluies_orageuses.svg', 'Orages': 'orages.svg',
-                         'Ciel voilé': 'ciel_voile.svg'}
+                         'Ciel voilé': 'ciel_voile.svg', 'Bancs de Brouillard': 'bancs_brouillard.svg'}
     try:
         pictogramme = available_weather[weather]
     except KeyError:
