1ef5e39debd467721790f6e51508c9de4df90d78
Olivier Henry <olivierpascalhenry>
2021-12-18 18:29:51 +0100

    collecting thread has been split into different threads : one thread per captor

--- database/create_weather_station_dabatase.sql
+++ database/create_weather_station_dabatase.sql
@@ -11,31 +11,11 @@ GRANT TEMPORARY, CONNECT ON DATABASE weather_station_db TO PUBLIC;
 -- se connecter à la base weather_station_db avant de dérouler la suite
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
--- creation des sequences
-/* CREATE SEQUENCE public.ext_temp_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-ALTER SEQUENCE public.ext_temp_id_seq OWNER TO weather_station;
-
-CREATE SEQUENCE public.int_hum_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-ALTER SEQUENCE public.int_hum_id_seq OWNER TO weather_station;
-
-CREATE SEQUENCE public.int_temp_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-ALTER SEQUENCE public.int_temp_id_seq OWNER TO weather_station;
-
-CREATE SEQUENCE public.int_pres_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
-ALTER SEQUENCE public.int_pres_id_seq OWNER TO weather_station; */
-
 -- creation des tables
-CREATE TABLE IF NOT EXISTS public.ext_temp (ext_tp_time timestamp without time zone NOT NULL, ext_tp_data real, CONSTRAINT ext_temp_pkey PRIMARY KEY (ext_tp_time)) TABLESPACE pg_default;
-ALTER TABLE public.ext_temp OWNER to weather_station;
+CREATE TABLE IF NOT EXISTS public."DS18B20" (date_time timestamp without time zone NOT NULL, temperature real, CONSTRAINT "DS18B20_pkey" PRIMARY KEY (date_time)) TABLESPACE pg_default;
+ALTER TABLE public."DS18B20" OWNER to weather_station;
+COMMENT ON TABLE public."DS18B20" IS 'Table dédiée au capteur DS18B20';
 
-CREATE TABLE IF NOT EXISTS public.int_hum (int_hd_time timestamp without time zone NOT NULL, int_hd_data real, CONSTRAINT int_hum_pkey PRIMARY KEY (int_hd_time)) TABLESPACE pg_default;
-ALTER TABLE public.int_hum OWNER to weather_station;
-
-CREATE TABLE IF NOT EXISTS public.int_temp (int_tp_time timestamp without time zone NOT NULL, int_tp_data real, CONSTRAINT int_temp_pkey PRIMARY KEY (int_tp_time)) TABLESPACE pg_default;
-ALTER TABLE public.int_temp OWNER to weather_station;
-
-CREATE TABLE IF NOT EXISTS public.int_pres (int_ps_time timestamp without time zone NOT NULL, int_ps_data real, CONSTRAINT int_pres_pkey PRIMARY KEY (int_ps_time)) TABLESPACE pg_default;
-ALTER TABLE public.int_pres OWNER to weather_station;
-
-CREATE TABLE IF NOT EXISTS public.int_pres_msl (int_ps_time timestamp without time zone NOT NULL, int_ps_data real, CONSTRAINT int_pres_msl_pkey PRIMARY KEY (int_ps_time)) TABLESPACE pg_default;
-ALTER TABLE public.int_pres_msl OWNER to weather_station;
+CREATE TABLE IF NOT EXISTS public."BME280" (date_time timestamp without time zone NOT NULL, temperature real, pression real, pression_msl real, humidite real, CONSTRAINT "BME280_pkey" PRIMARY KEY (date_time)) TABLESPACE pg_default;
+ALTER TABLE public."BME280" OWNER to weather_station;
+COMMENT ON TABLE public."BME280" IS 'Table dédiée au capteur BME280';
--- functions/thread_functions/sensors_reading.py
+++ functions/thread_functions/sensors_reading.py
@@ -10,55 +10,35 @@ if platform.system() == 'Linux':
     import bme280
 
 
-class DataCollectingThread(QtCore.QThread):
+class DS18B20DataCollectingThread(QtCore.QThread):
     error = QtCore.pyqtSignal(list)
 
     def __init__(self, connector, cursor, config_dict):
         QtCore.QThread.__init__(self)
-        logging.debug('gui - sensors_reading.py - DataCollectingThread - __init__')
+        logging.debug('gui - sensors_reading.py - DS18B20DataCollectingThread - __init__')
         self.connector = connector
         self.cursor = cursor
-        self.cal_params = None
-        self.bus = None
-        self.address = None
         self.sensors_rate = int(config_dict.get('SYSTEM', 'sensors_rate'))
-        if config_dict.get('SYSTEM', 'place_altitude'):
-            self.place_altitude = int(config_dict.get('SYSTEM', 'place_altitude'))
-        else:
-            self.place_altitude = None
 
     def run(self):
-        logging.debug('gui - sensors_reading.py - DataCollectingThread - run')
-        if platform.system() == 'Linux' and platform.node() != 'raspberry':
-            try:
-                self.bus = smbus2.SMBus(1)
-                self.address = 0x77
-                self.cal_params = self.prepare_bme280_bus()
-            except Exception as e:
-                self.error.emit(['data collect', e])
         while True:
-            date_time = datetime.datetime.now().replace(microsecond=0)
+            dtime = datetime.datetime.now().replace(microsecond=0)
             try:
-                ext_tp = self.collect_ext_temp()
-                int_tp, int_hd, int_ps, int_ps_msl = self.collect_int_temp_hum_pres()
-                self.add_data_to_db(date_time, int_tp, ext_tp, int_hd, int_ps, int_ps_msl)
+                temp = self.collect_data()
+                self.add_data_to_db(dtime, temp)
                 time.sleep(self.sensors_rate)
             except Exception as e:
-                self.error.emit(['data collect', e])
-                self.add_data_to_db(date_time, None, None, None, None, None)
+                self.error.emit(['DS18B20 data collecting', e])
+                self.add_data_to_db(dtime, None)
 
-    def add_data_to_db(self, dt, int_tp, ext_tp, int_hd, int_ps, int_ps_msl):
+    def add_data_to_db(self, dt, tp):
         try:
-            self.cursor.execute('insert into int_temp (int_tp_time, int_tp_data) values (%s, %s)', (dt, int_tp))
-            self.cursor.execute('insert into ext_temp (ext_tp_time, ext_tp_data) values (%s, %s)', (dt, ext_tp))
-            self.cursor.execute('insert into int_hum (int_hd_time, int_hd_data) values (%s, %s)', (dt, int_hd))
-            self.cursor.execute('insert into int_pres (int_ps_time, int_ps_data) values (%s, %s)', (dt, int_ps))
-            self.cursor.execute('insert into int_pres_msl (int_ps_time, int_ps_data) values (%s, %s)', (dt, int_ps_msl))
+            self.cursor.execute('insert into "DS18B20" (date_time, temperature) values (%s, %s)', (dt, tp))
             self.connector.commit()
         except (psycopg2.InterfaceError, psycopg2.OperationalError):
             pass
 
-    def collect_ext_temp(self):
+    def collect_data(self):
         temp = None
         if platform.system() == 'Linux' and platform.node() != 'raspberry':
             try:
@@ -77,7 +57,56 @@ class DataCollectingThread(QtCore.QThread):
             temp = self.collect_test_data(25., 5)
         return temp
 
-    def collect_int_temp_hum_pres(self):
+    @staticmethod
+    def collect_test_data(num, limit):
+        random.seed()
+        if random.randrange(1, 7, 1) == 1:
+            return None
+        else:
+            return round(num + random.uniform(0, limit), 1)
+
+    def stop(self):
+        logging.debug('gui - sensors_reading.py - DS18B20DataCollectingThread - stop')
+        self.terminate()
+
+
+class BME280DataCollectingThread(QtCore.QThread):
+    error = QtCore.pyqtSignal(list)
+
+    def __init__(self, connector, cursor, config_dict):
+        QtCore.QThread.__init__(self)
+        logging.debug('gui - sensors_reading.py - BME280DataCollectingThread - __init__')
+        self.connector = connector
+        self.cursor = cursor
+        self.sensors_rate = int(config_dict.get('SYSTEM', 'sensors_rate'))
+        self.cal_params = None
+        self.bus = None
+        self.address = None
+        if config_dict.get('SYSTEM', 'place_altitude'):
+            self.place_altitude = int(config_dict.get('SYSTEM', 'place_altitude'))
+        else:
+            self.place_altitude = None
+
+    def run(self):
+        logging.debug('gui - sensors_reading.py - BME280DataCollectingThread - run')
+        if platform.system() == 'Linux' and platform.node() != 'raspberry':
+            try:
+                self.bus = smbus2.SMBus(1)
+                self.address = 0x77
+                self.cal_params = self.prepare_bme280_bus()
+            except Exception as e:
+                self.error.emit(['BME280 data collecting', e])
+        while True:
+            date_time = datetime.datetime.now().replace(microsecond=0)
+            try:
+                temp, hum, pres, pres_msl = self.collect_data()
+                self.add_data_to_db(date_time, temp, hum, pres, pres_msl)
+                time.sleep(self.sensors_rate)
+            except Exception as e:
+                self.error.emit(['BME280 data collecting', e])
+                self.add_data_to_db(date_time, None, None, None, None)
+
+    def collect_data(self):
         if platform.system() == 'Linux' and platform.node() != 'raspberry':
             data = bme280.sample(self.bus, self.address, self.cal_params)
             temp = data.temperature
@@ -101,21 +130,27 @@ class DataCollectingThread(QtCore.QThread):
             pres_msl = self.collect_test_data(1013., 15)
         return temp, hum, pres, pres_msl
 
-    @staticmethod
-    def collect_test_data(num, limit):
-        random.seed()
-        none_num = random.randrange(1, 7, 1)
-        if none_num == 1:
-            data = None
-        else:
-            data = round(num + random.uniform(0, limit), 1)
-        return data
+    def add_data_to_db(self, dt, tp, hm, ps, ps_msl):
+        try:
+            self.cursor.execute('insert into "BME280" (date_time, temperature, humidite, pression, pression_msl) '
+                                'values (%s, %s, %s, %s, %s)', (dt, tp, hm, ps, ps_msl))
+            self.connector.commit()
+        except (psycopg2.InterfaceError, psycopg2.OperationalError):
+            pass
 
     def prepare_bme280_bus(self):
         return bme280.load_calibration_params(self.bus, self.address)
 
+    @staticmethod
+    def collect_test_data(num, limit):
+        random.seed()
+        if random.randrange(1, 7, 1) == 1:
+            return None
+        else:
+            return round(num + random.uniform(0, limit), 1)
+
     def stop(self):
-        logging.debug('gui - sensors_reading.py - DataCollectingThread - stop')
+        logging.debug('gui - sensors_reading.py - BME280DataCollectingThread - stop')
         self.terminate()
 
 
@@ -133,64 +168,63 @@ class DBDataDisplayThread(QtCore.QThread):
         logging.debug('gui - sensors_reading.py - DBDataDisplayThread - run')
         time.sleep(self.display_rate / 2.)
 
-        q_temp_norm_in = 'SELECT int_tp_time, int_tp_data FROM int_temp ORDER BY int_tp_time DESC LIMIT 1'
-        q_temp_minmax_in = 'SELECT MIN (int_tp_data), MAX (int_tp_data) FROM int_temp'
-        q_temp_norm_out = 'SELECT ext_tp_data FROM ext_temp ORDER BY ext_tp_time DESC LIMIT 1'
-        q_temp_minmax_out = 'SELECT MIN (ext_tp_data), MAX (ext_tp_data) FROM ext_temp'
-        q_hum_norm_in = 'SELECT int_hd_data FROM int_hum ORDER BY int_hd_time DESC LIMIT 1'
-        # q_hum_minmax_in = 'SELECT MIN (int_hd_data), MAX (int_hd_data) FROM int_hum'
-        q_pres_norm_in = 'SELECT int_ps_data FROM int_pres ORDER BY int_ps_time DESC LIMIT 1'
-        q_presmsl_norm_in = 'SELECT int_ps_data FROM int_pres_msl ORDER BY int_ps_time DESC LIMIT 1'
+        temp_in = 'SELECT date_time, temperature FROM "BME280" ORDER BY date_time DESC LIMIT 1'
+        temp_minmax_in = 'SELECT MIN (temperature), MAX (temperature) FROM "BME280"'
+        temp_out = 'SELECT temperature FROM "DS18B20" ORDER BY date_time DESC LIMIT 1'
+        temp_minmax_out = 'SELECT MIN (temperature), MAX (temperature) FROM "DS18B20"'
+        hum_in = 'SELECT humidite FROM "BME280" ORDER BY date_time DESC LIMIT 1'
+        pres_in = 'SELECT pression FROM "BME280" ORDER BY date_time DESC LIMIT 1'
+        presmsl_in = 'SELECT pression_msl FROM "BME280" ORDER BY date_time DESC LIMIT 1'
 
         while True:
             try:
-                temp_dict = {'temp_norm_in': None, 'temp_minmax_in': None, 'temp_norm_out': None,
-                             'temp_minmax_out': None, 'hum_norm_in': None, 'pres_norm_in': None,
-                             'presmsl_norm_in': None, 'datetime': None}
+                temp_dict = {'temp_in': None, 'temp_minmax_in': None, 'temp_out': None,
+                             'temp_minmax_out': None, 'hum_in': None, 'pres_in': None,
+                             'presmsl_in': None, 'datetime': None}
                 try:
-                    self.cursor.execute(q_temp_norm_in)
+                    self.cursor.execute(temp_in)
                     data = self.cursor.fetchone()
-                    temp_dict['temp_norm_in'] = data[1]
+                    temp_dict['temp_in'] = data[1]
                     temp_dict['datetime'] = data[0]
                 except psycopg2.ProgrammingError:
-                    temp_dict['temp_norm_in'] = None
+                    temp_dict['temp_in'] = None
                     temp_dict['datetime'] = None
                 try:
-                    self.cursor.execute(q_temp_minmax_in)
+                    self.cursor.execute(temp_minmax_in)
                     data = self.cursor.fetchone()
                     temp_dict['temp_minmax_in'] = data
                 except psycopg2.ProgrammingError:
                     temp_dict['temp_minmax_in'] = None
                 try:
-                    self.cursor.execute(q_temp_norm_out)
+                    self.cursor.execute(temp_out)
                     data = self.cursor.fetchone()
-                    temp_dict['temp_norm_out'] = data[0]
+                    temp_dict['temp_out'] = data[0]
                 except psycopg2.ProgrammingError:
-                    temp_dict['temp_norm_out'] = None
+                    temp_dict['temp_out'] = None
                 try:
-                    self.cursor.execute(q_temp_minmax_out)
+                    self.cursor.execute(temp_minmax_out)
                     data = self.cursor.fetchone()
                     temp_dict['temp_minmax_out'] = data
                 except psycopg2.ProgrammingError:
                     temp_dict['temp_minmax_out'] = None
                 try:
-                    self.cursor.execute(q_hum_norm_in)
+                    self.cursor.execute(hum_in)
                     data = self.cursor.fetchone()
-                    temp_dict['hum_norm_in'] = data[0]
+                    temp_dict['hum_in'] = data[0]
                 except psycopg2.ProgrammingError:
-                    temp_dict['hum_norm_in'] = None
+                    temp_dict['hum_in'] = None
                 try:
-                    self.cursor.execute(q_pres_norm_in)
+                    self.cursor.execute(pres_in)
                     data = self.cursor.fetchone()
-                    temp_dict['pres_norm_int'] = data[0]
+                    temp_dict['pres_in'] = data[0]
                 except psycopg2.ProgrammingError:
-                    temp_dict['pres_norm_int'] = None
+                    temp_dict['pres_in'] = None
                 try:
-                    self.cursor.execute(q_presmsl_norm_in)
+                    self.cursor.execute(presmsl_in)
                     data = self.cursor.fetchone()
-                    temp_dict['presmsl_norm_int'] = data[0]
+                    temp_dict['presmsl_in'] = data[0]
                 except psycopg2.ProgrammingError:
-                    temp_dict['presmsl_norm_int'] = None
+                    temp_dict['presmsl_in'] = None
                 self.db_data.emit(temp_dict)
             except Exception as e:
                 self.error.emit(['data display', e])
